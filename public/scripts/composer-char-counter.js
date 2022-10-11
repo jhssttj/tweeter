0cfe2294efd96596238d02feb60e9a81e05bd53a
Kevin Lee <jhssttj@hotmail.com>
2022-10-11 14:54:02 -0600

    Change tweet count display color to red if over 140 char limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,11 +4,14 @@ $(document).ready(function() {
   console.log("read function good");
   let tweetText = $("#tweet-text");
   tweetText.on("input", function () {
-    let charCount = $(".counter");
+    let charCount = $("#char_count");
     let limitText = 140;
     const tweetLength = $(this).val().length;
     let maxLength = limitText - tweetLength;
     charCount.text(maxLength);
+    if (maxLength < 0) {
+      charCount.css({ 'color'  : 'red'});
+    }
   })  
 });
 
