587d862c2fb43be5afd9e8ec34e9432ac9e25c3b
Kevin Lee <jhssttj@hotmail.com>
2022-10-12 11:35:51 -0600

    Play around script positioning and document read (No major changes, just experiments).

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,42 +1,43 @@
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
+$(document).ready(function() {
+  // Object of tweet datas
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+    "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+    "created_at": 1461116232227
+  };
 
-//Function to return article template for newly posted tweets
-createTweetElement = (tweetData) => {
-  let renderPost = (`
-  <section>
+  createTweetElement = function (tweet) {
+    const $tweet = $(`
     <article>
-      <header class = "PostedTweets">
-        <img src="${tweetData.user.avatars}"> 
-        <h3>${tweetData.user.name}</h3>
-        <h3 class = "atName">${tweetData.user.handle}</h3>
-      </header>
-      <p>${tweetData.content.text}</p>
-      <footer> 
-        Posted on ${tweetData.created_at}
-        <span>
-          <i class="fa-solid fa-flag"></i>
-          <i class="fa-solid fa-retweet"></i>
-          <i class="fa-solid fa-heart"></i>
-        </span>
-      </footer>
-    </article>
-  </section>`)
-  return renderPost;
-};
+    <header class = "PostedTweets">
+      <img src="${tweet.user.avatars}"> 
+      <h3>${tweet.user.name}</h3>
+      <h3 class = "atName">${tweet.user.handle}</h3>
+    </header>
+    <p>${tweet.content.text}</p>
+    <footer> 
+    ${tweet.created_at}
+      <span>
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </span>
+    </footer>
+  </article>
+    `);
+    return $tweet;
+  };
 
-const $tweet = createTweetElement(tweetData);
+  const $tweet = createTweetElement(tweetData);
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  // Test / driver code (temporary)
+  console.log('test123131')
+  console.log($tweet); // to see what it looks like
+  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+});
