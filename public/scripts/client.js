a866dd48b64ec39ac3a38e4ef0053133ad689386
Kevin Lee <jhssttj@hotmail.com>
2022-10-13 12:16:50 -0600

    Eslint fixes and add comment detailing code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   //Function to render individual tweet into the article template
-  const createTweetElement = function (tweet) {
+  const createTweetElement = function(tweet) {
     const $tweet = $(`
     <article>
     <header class = "PostedTweets">
@@ -17,25 +17,27 @@ $(document).ready(function() {
         <i class="fa-solid fa-heart"></i>
       </span>
     </footer>
-  </article>
-    `);
+  </article>`);
     return $tweet;
   };
-
-  //Initially hide error message for tweet word limit
-  $('.error').hide();
-  $('.new-tweet').hide();
-
+  
   //Function to take array of tweets and render them using the createTweetElement function
   const renderTweets = function(tweets) {
     for (const tweet of tweets) {
       const $tweet = createTweetElement(tweet);
-      $('#tweets-container').prepend($tweet)
+      $('#tweets-container').prepend($tweet);
     }
   };
 
+  //Function below escapes the text to prevent XSS issues
+  const escape = function(str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
   //Function to load all the tweet in our database
-  const loadtweets = function () {
+  const loadtweets = function() {
     $.ajax({
       method: 'GET',
       url: '/tweets',
@@ -45,24 +47,15 @@ $(document).ready(function() {
     })
     .catch((error) => {
       console.log ('ERROR:', error);
-    })
+    });
   };
 
-  loadtweets();
-
-  //Function below escapes the text to prevent XSS issues
-  const escape = function (str) {
-    let div = document.createElement("div");
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-  };
-
-  //Ajax: Put posted tweet into server once submitted
-  $( "#tweetForm" ).submit(( event ) => {
+  //Post new tweet into the post when user submits a new tweet
+  $("#tweetForm").submit((event) => {
     event.preventDefault();
     const newTweet = $('#tweetForm').serialize();
     let tweetLength = $(this).find('textarea').val().length;
-    const charCount = $(this).parent().find('.counter');
+    //Hide error sign before user submits and show correct error based on invalid tweet
     $('.error').hide();
     if (!tweetLength) {
       return $('#error_empty').slideDown();
@@ -70,25 +63,31 @@ $(document).ready(function() {
     if (tweetLength > 140) {
       return $('#error_over').slideDown();
     }
-
+    //Send data when valid tweet gets posted. Update tweet data base and render new tweets
     $.ajax({
       method: 'POST',
       url: '/tweets',
       data: newTweet
     })
-    .then (() => {
+    .then(() => {
       $("#tweets-container").empty();
       loadtweets();
     })
     .catch((error) => {
-      console.log ('ERROR:', error);
+      console.log('ERROR:', error);
     })
+    //Reset new tweet form for future tweets
     $(this).find('textarea').val('');
     $(this).find('.counter').val(140);
-  }); 
+  });
 
-  //Ajax: Click on icon slides the new tweer header up and down
+  //Code below toggles the new tweet article by clicking icon on nav panel
   $(".fa-angles-down").click(() => {
     $('.new-tweet').slideToggle();
-  }); 
+  });
+
+  //Initially hide error message for tweet word limit and load previously posted tweets
+  $('.error').hide();
+  $('.new-tweet').hide();
+  loadtweets();
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,25 +1,26 @@
 $(document).ready(function() {
-  console.log("read function good");
+  //Determine tweet length as input types and display amount of text used
   let tweetText = $("#tweet-text");
-  tweetText.on("input", function () {
+  tweetText.on("input", function() {
     const charCount = $(this).parent().find('.counter');
     const tweetLength = $(this).val().length;
     let maxLength = 140 - tweetLength;
     charCount.text(maxLength);
-    //If word limit goes down to negative. Change font color to red
+    //If word limit goes down to negative. Change class that dictate CSS font color to red
     if (maxLength < 0) {
-      // charCount.css({ 'color'  : 'red'});
       charCount.addClass('warningColor');
     } else if (maxLength > 0) {
       charCount.removeClass('warningColor');
     }
-  })
-  
+  });
 
-
+  //Show or hide button that scrolls to top of page based on web position
   let scrollToTop = $('#scrollToTop');
   let navTemplate = $('nav');
+  //Initially hide the scroll button
   scrollToTop.hide();
+  // Show scroll button and hide nav panel when not on top of page
+  // Else, hide scroll button and show nav panel when on top of page
   $(window).scroll(() => {
     if (window.scrollY > 0) {
       scrollToTop.fadeIn();
@@ -27,16 +28,15 @@ $(document).ready(function() {
     } else {
       scrollToTop.fadeOut();
       navTemplate.slideDown();
-
     }
-  })
+  });
+  // Clicking on the scroll button brings you back to top of web page
   scrollToTop.click(() => {
     window.scrollTo({
       top: 0,
       left: 0,
       behavior: "smooth"
-    })
-
-  })
+    });
+  });
 });
 
